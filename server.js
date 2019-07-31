65876380728a9119bff294d6972df33643c0cbe5
Eric Lara <eric.lara@axa.com>
2019-07-31 20:58:10 +0200

    feat(env): take env vars from dotenv

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
+const path = require('path');
 const crypto = require('crypto');
-const express = require("express");
-const path = require("path");
-const jwt = require("jsonwebtoken");
-const rp = require("request-promise");
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const rp = require('request-promise');
 const cookieParser = require('cookie-parser');
 
 // Initialize the web app instance,
@@ -10,11 +12,11 @@ const app = express();
 app.use(cookieParser());
 // Indicate which directory static resources
 // (e.g. stylesheets) should be served from.
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 // begin listening for requests.
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
-    console.log("Express server listening on port " + port);
+    console.log('Express server listening on port ' + port);
 });
 
 function isUserAuthenticated(){
@@ -35,28 +37,34 @@ app.get('/chatBot',  function(req, res) {
         },
         json: true
     };
-    rp(options)
-        .then(function (parsedBody) {
-            var userid = req.query.userId || req.cookies.userid;
-            if (!userid) {
-                userid = crypto.randomBytes(4).toString('hex');
-                res.cookie("userid", userid);
-            }
+    console.log('options -> ', options);
+    rp(options).then(function (parsedBody) {
+        let userId = req.query.userId || req.cookies.userid;
+        if (!userId) {
+            userId = crypto.randomBytes(4).toString('hex');
+            res.cookie('userid', userId);
+        }
 
-            var response = {};
-            response['userId'] = userid;
-            response['userName'] = req.query.userName;
-            response['connectorToken'] = parsedBody.token;
-            response['optionalAttributes'] = {age: 33};
-            if (req.query.lat && req.query.long)  {
-                response['location'] = {lat: req.query.lat, long: req.query.long};
-            }
-            response['directLineURI'] = process.env.DIRECTLINE_ENDPOINT_URI;
-            const jwtToken = jwt.sign(response, process.env.APP_SECRET);
-            res.send(jwtToken);
-        })
-        .catch(function (err) {
-            res.status(err.statusCode).send();
-            console.log("failed");
-        });
+        const response = {
+            userId,
+            userName: req.query.userName,
+            connectorToken: parsedBody.token,
+            /*
+            optionalAttributes: {
+                age: 33
+            }, 
+            */
+            directLineURI: process.env.DIRECTLINE_ENDPOINT_URI
+        };
+
+        if (req.query.lat && req.query.long)  {
+            response.location = { lat: req.query.lat, long: req.query.long };
+        }
+        const jwtToken = jwt.sign(response, process.env.APP_SECRET);
+        res.send(jwtToken);
+    }).catch(function (err) {
+        console.error('err -> ', err);
+        res.status(err.statusCode).send();
+        console.log('failed');
+    });
 });
